f8f9ea527f25e68986095deacd4f9d323a850a09
awe.wang <awe31402@gmail.com>
2017-03-22 19:44:12 +0800

    [MACE] Remove resundant comment

--- mace_filter/mace_filter.m
+++ mace_filter/mace_filter.m
@@ -7,17 +7,14 @@ img = imread(filenames(1).name);
 [row, col] = size(img);
 
 I = imread('target.jp2');
-%I = rgb2gray(I);
+I = rgb2gray(I);
 [targetRow, targetCol] = size(I);
 targetRow = 2^nextpow2(targetRow + row - 1);
 targetCol = 2^nextpow2(targetCol + col - 1);
 
-%figure;
 for i = 1 : num_files
     printf('%s\n', filenames(i).name);
     img = imread(filenames(i).name);
-    %subplot(5 ,4, i);
-    %imshow(img);
     img = rgb2gray(img);
     img = double(img);
     img = fft2(img, targetRow, targetCol);
@@ -31,25 +28,15 @@ u = 10 ^ 6 * ones(num_files, 1);
 D_inv = inv(D);
 H = D_inv * X * inv(X' * D_inv * X) * u;
 H_mace = reshape((H), [targetRow, targetCol]);
-%H_tmp = zeros(targetRow, targetCol);
-%H_tmp(1: row, 1:col) = H_mace(1: row, 1:col);
-%H_mace = H_tmp;
 
 % Verify face recognution
 I = double(I);
 m = fft2(I, targetRow, targetCol);
-%H_mace_p = zeros(size(m));
-%H_mace_p(1: row, 1 : col) = H_mace;
+
 k = m .* conj(H_mace);
-%figure;
-%mesh(abs(k));
+
 g = ifft2(k);
 figure;
-%begR = floor((targetRow - size(I)(1)) /2);
-%begC = floor((targetCol - size(I)(2)) /2);
-%endR = floor((targetRow + size(I)(1)) /2);
-%endC = floor((targetCol + size(I)(2)) /2);
-%g = abs(g(begR : endR, begC:endC));
 g = abs(g(1:size(I)(1), 1:size(I)(2)));
 mesh(g);
 
