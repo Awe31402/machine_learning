f64781b1dbd4cd60443e69986890109783cab9fa
awe.wang <awe31402@gmail.com>
2017-04-13 19:49:35 +0800

    [SVM:TestProces] combine two test process to run once

--- taiwan_face/runtest.sh
+++ taiwan_face/runtest.sh
@@ -2,17 +2,14 @@
 female_file=`ls f*.jpg`
 male_file=`ls m*.jpg`
 
-
-#test_gender='test.jpg'
-#test_files=$female_file
-
 test_gender='test.jpg'
 test_files=$male_file
+test_files+=" "$female_file
 
 for i in $test_files
 do
     mv $i $test_gender
     echo "testing $i.."
-    time octave main.m
+    octave main.m
     mv $test_gender $i
 done
